c9d85d65d1e762ebbccf4487ec2619f106d5f304
DENIS FERMINO DE SOUZA <denis.fermino@embraer.com.br>
2025-07-07 20:52:14 -0300

    melhorando o codigo

--- Exerc_04.py
+++ Exerc_04.py
@@ -2,20 +2,25 @@ from datetime import date
 
 nasc = int(input(' Qual a data de nascimento? '))
 ano_atual = date.today().year
+sexo = input('''Qual seu genero: 
+             [M] Masculino
+             [F] Feminino:  ''')
 
 idade = ano_atual - nasc
 
-if idade > 18:
+if sexo == 'M' and idade > 18:
     anos = idade - 18
     print(f'Você está com {idade} anos e já deveria ter se alistado há {anos} anos atrás, no ano de {ano_atual - anos}')
 
 
-elif idade == 18:
+elif sexo == 'M' and idade == 18:
     anos = idade - 18
     print(f'Você está com {idade} anos e já deve se alistar esse ano de {ano_atual - anos}')
 
-elif idade < 18:
+elif sexo == 'M' and idade < 18:
     saldo = 18 - idade
     ano = ano_atual + saldo
     print(f'Você está com {idade} anos e ainda falta {saldo} anos para vc se alistar, no ano de {ano}')    
 
+else:
+    print('Você é Mulher e não precisa de alistar')
