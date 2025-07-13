a3956d3cd2178d33c17b9b475f2f325c762be0bb
Souzadfs <souzadf09@gmail.com>
2025-07-13 19:46:12 -0300

    Exerc_8

--- Repetição in For/Exerc_08.py
+++ Repetição in For/Exerc_08.py
@@ -1,10 +1,15 @@
 frase = str(input('Digite a Frase: ')).strip().upper()
 palvras = frase.split()
 junto = ''.join(palvras)
+inverso = ''
 
 for i in range(len(junto) - 1, -1, -1):
-    print(junto[i], end='')
-
-
-
-    # print(f'A frase que você digitou é {frase}, {junto}, {palvras}')
+    #print(junto[i], end='')
+    inverso += junto[i]
+   
+if inverso == junto:
+    print(f'Frase que vc digitou é um palindromo {junto},{inverso}')
+    
+else:
+    print(f'Frase que vc digitou NÂO é um palindromo {junto},{inverso}')
+      # print(f'A frase que você digitou é {frase}, {junto}, {palvras}')
