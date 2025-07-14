3d15dd7f9876cc4e1f1630ac283fb20febaf0f3f
Souzadfs <souzadf09@gmail.com>
2025-07-14 05:26:25 -0300

    Exerc 9

--- Repetição in For/Exerc_09.py
+++ Repetição in For/Exerc_09.py
@@ -2,8 +2,22 @@
 # mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
 
 from datetime import date
-ano_atual = date.today()
+ano_atual = date.today().year
+totmaior = 0
+totmenor = 0
 
 for i in range(1, 8):
     nasc = int(input('Qual seu ano de nascimento? '))
+    idade = ano_atual -nasc
     
+    if idade >= 21:
+        print(f' {idade} Maior idade')
+        totmaior += 1
+        
+    
+    else:
+        print(f' {idade} Menor idade')
+        totmenor += 1
+
+print(f'A quantidade de pessoa de Maior idade é {totmaior}')
+print(f'A quantidade de pessoa de Menor idade é {totmenor}')
