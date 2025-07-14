59252365e19973b79a1f3379cbcd3bb49aa3f233
Souzadfs <souzadf09@gmail.com>
2025-07-14 20:05:51 -0300

    Exerc 11

---
+++ Repetição in For/Exerc_11.py
@@ -0,0 +1,11 @@
+#Exercício Python 11: Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, 
+# mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+
+
+
+somaidade = 0
+for i in range(1, 5):
+    idade = int(input('Qual é a idade'))
+    nome = str(input('Qual nome')).strip()
+    sexo = str(input('Qual Sexo? [M / F] ? '))
+    somaidade += idade
