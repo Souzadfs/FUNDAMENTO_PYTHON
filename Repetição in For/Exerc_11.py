f9df8a116e58cbd1d5c34cdfa89bee93b2e01076
Souzadfs <souzadf09@gmail.com>
2025-07-14 20:20:54 -0300

    evoluindo

--- Repetição in For/Exerc_11.py
+++ Repetição in For/Exerc_11.py
@@ -2,10 +2,18 @@
 # mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
 
 
-
+mediaidade = 0
 somaidade = 0
+maioridadehomen = 0
+nomevelho = ''
+
 for i in range(1, 5):
-    idade = int(input('Qual é a idade'))
-    nome = str(input('Qual nome')).strip()
-    sexo = str(input('Qual Sexo? [M / F] ? '))
+    idade = int(input('Qual é a idade: '))
+    nome = str(input('Qual nome: ')).strip()
+    sexo = str(input('Qual Sexo? [M / F] ?  '))
     somaidade += idade
+
+    if i == 1 and sexo in 'Mm':
+        maioridadehomen = idade
+        nomevelho = nome
+mediaidade = idade / 4
