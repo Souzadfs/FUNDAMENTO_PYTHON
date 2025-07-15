8c328ef4fecc1f47e9bd8d2311eecb25084ddeb8
Souzadfs <souzadf09@gmail.com>
2025-07-15 05:38:10 -0300

    Aula While

--- Repetição While/Aula_Wilhe.py
+++ Repetição While/Aula_Wilhe.py
@@ -1,5 +1,11 @@
-c = 0
-while c < 10:
-    c +=1
-    print(c, end=' ')
+n = 1
+par = impar = 0
+
+while n != 0:
+    n = int(input('Digite o valor: '))
+    if n %2 == 0:
+        par += 1
+    else:
+        impar += 1
+print(f'A quantidade de número par é {par}, e de impar é {impar} ')
 print('FIM')
