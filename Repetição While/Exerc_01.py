326788eee30db06019849e9a11c87e9f4ec7320b
Souzadfs <souzadf09@gmail.com>
2025-07-16 05:07:38 -0300

    Corrigido

--- Repetição While/Exerc_01.py
+++ Repetição While/Exerc_01.py
@@ -1,8 +1,8 @@
 #Exercício Python 01: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’. Caso esteja errado, 
 # peça a digitação novamente até ter um valor correto.
 
-genero = str(input('Qual seu sexo? [M/F] ')).strip().upper[0]
+genero = str(input('Qual seu sexo? [M/F] ')).strip().upper()[0]
 
 while genero not in 'MmFf':
-    genero = str(input('Dados invalidos'))
+    genero = str(input('Dados invalidos: '))
 print(f'O sexo digitado foi {genero}')
