de425a14f4dcdefa20fef17a8101316e83c9cf17
Souzadfs <souzadf09@gmail.com>
2025-07-22 05:44:41 -0300

    Melhorando o codigo

--- CURSO PYTHON UDEMY/Repetição For/Exercicio_01.py
+++ CURSO PYTHON UDEMY/Repetição For/Exercicio_01.py
@@ -1,9 +1,12 @@
 
 palavra_secreta = 'perfume'
 letra_acertada =''
+tentativa = 0
 
 while True:
     palavra = input('Digite uma letra: ')
+    tentativa += 1
+
     if len(palavra) > 1:
         print('Digite apenas uma letra')
         continue
@@ -14,11 +17,16 @@ while True:
    
     palavra_formada = ''
     for letra in palavra_secreta:
-        if letra in palavra_secreta:
-            palavra_formada += palavra_secreta
+        if letra in letra_acertada:
+            palavra_formada += letra
 
         else:
-            palavra_secreta =+ '*'
+            palavra_formada += '*'
         
-        
-        
+    print(f'A palavra formada é: {palavra_formada}')   
+
+    if palavra_formada == palavra_secreta:
+        print(palavra_formada)
+        print(tentativa)
+        letra_acertada =''
+        tentativa = 0
