a9d939489d8abf6014d4b107acbb0df3eab279aa
Souzadfs <souzadf09@gmail.com>
2025-07-23 05:18:14 -0300

    Construindo a lógica

--- CURSO EM VIDEO/Repetição While/Exerc_03.py
+++ CURSO EM VIDEO/Repetição While/Exerc_03.py
@@ -6,14 +6,23 @@
 while True:
     primeiro_valor = float(input('Digite o primeiro valor :'))
     segundo_valor = float(input('Digite o segundo valor :'))
-    print('''[ 1 ] Somar' 
-             [ 2 ] Mutiplicar' 
-             [ 3 ] Maior' 
-             [ 4 ] Novo numero' 
-             [ 5 ] Sair do Programa''')
+    print(''' [ 1 ] Somar' 
+              [ 2 ] Mutiplicar' 
+              [ 3 ] Maior' 
+              [ 4 ] Novo numero' 
+              [ 5 ] Sair do Programa''')
     opcao = int(input ('>>>>>  Qual a sua opção? '))
 
     if opcao == 1:
        soma = primeiro_valor + segundo_valor
-       print(soma)
-        
+       print(f'A soma entre o {primeiro_valor} + {segundo_valor} = {soma}')
+
+    elif opcao == 2:
+        multiplicar = primeiro_valor * segundo_valor
+        print(f'A Multiplicação entre {primeiro_valor} * {segundo_valor} = {multiplicar}')
+    
+    elif opcao == 3:
+        if primeiro_valor > segundo_valor:
+            print(f'O Primeiro valor {primeiro_valor} é Maior que o {segundo_valor}') 
+        elif primeiro_valor < segundo_valor:
+            print(f'O Segundo valor {segundo_valor} é Maior que o {primeiro_valor}')
