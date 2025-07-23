d7904f2b60036a1af6ba3d383eb92ad9a20e5312
Souzadfs <souzadf09@gmail.com>
2025-07-23 05:06:14 -0300

    Construindo o exerc

--- CURSO EM VIDEO/Repetição While/Exerc_03.py
+++ CURSO EM VIDEO/Repetição While/Exerc_03.py
@@ -1,5 +1,19 @@
-#Exercício Python 059: Crie um programa que leia dois valores e mostre um menu na tela:
+#Exercício Python 03: Crie um programa que leia dois valores e mostre um menu na tela:
 #[ 1 ] somar
 
 #[ 2 ] multiplicar
 
+while True:
+    primeiro_valor = float(input('Digite o primeiro valor :'))
+    segundo_valor = float(input('Digite o segundo valor :'))
+    print('''[ 1 ] Somar' 
+             [ 2 ] Mutiplicar' 
+             [ 3 ] Maior' 
+             [ 4 ] Novo numero' 
+             [ 5 ] Sair do Programa''')
+    opcao = int(input ('>>>>>  Qual a sua opção? '))
+
+    if opcao == 1:
+       soma = primeiro_valor + segundo_valor
+       print(soma)
+        
