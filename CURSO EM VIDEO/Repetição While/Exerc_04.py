c586935ef1aecd467127f9bb820d044bf19a3505
Souzadfs <souzadf09@gmail.com>
2025-07-24 05:34:19 -0300

    Exerc feito sem import

--- CURSO EM VIDEO/Repetição While/Exerc_04.py
+++ CURSO EM VIDEO/Repetição While/Exerc_04.py
@@ -1,10 +1,23 @@
 #Exercício Python 04: Faça um programa que leia um número qualquer e mostre o seu fatorial.
 
-from math import factorial
+#from math import factorial
 
 
-while True:
-    numero = int(input('Digite um numero: '))
-    f = factorial(numero)
-    print(numero, f)
+#while True:
+#    numero = int(input('Digite um numero: '))
+#    f = factorial(numero)
+#    print(numero, f)
 
+
+numero = int(input('Digite um numero: '))
+c = numero
+f = 1
+
+while  c > 0:
+    print(f'{c}', end=' ')
+    print('x ' if c > 1 else '=', end=' ')
+   
+    f *= c
+    c -= 1
+
+print(f)
