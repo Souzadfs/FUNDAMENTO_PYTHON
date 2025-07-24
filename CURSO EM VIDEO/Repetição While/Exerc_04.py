43d2facf0763b29a08455015c207649e67dc8645
Souzadfs <souzadf09@gmail.com>
2025-07-24 05:07:17 -0300

    1° tentativa

--- CURSO EM VIDEO/Repetição While/Exerc_04.py
+++ CURSO EM VIDEO/Repetição While/Exerc_04.py
@@ -1,1 +1,10 @@
 #Exercício Python 04: Faça um programa que leia um número qualquer e mostre o seu fatorial.
+
+
+fatorial = 0
+
+while True:
+    numero = int(input('Digite um numero: '))
+    fatorial *= 1
+    print(fatorial)
+
