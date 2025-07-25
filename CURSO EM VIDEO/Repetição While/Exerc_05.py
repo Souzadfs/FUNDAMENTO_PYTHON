ee31e7b9bc32a72a683a42384de6c8db764b76dc
Souzadfs <souzadf09@gmail.com>
2025-07-25 19:21:02 -0300

    Add tilte

--- CURSO EM VIDEO/Repetição While/Exerc_05.py
+++ CURSO EM VIDEO/Repetição While/Exerc_05.py
@@ -1,3 +1,6 @@
+#Exercício Python 61: Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, 
+# mostrando os 10 primeiros termos da progressão usando a estrutura while.
+
 
 primeiro = int(input("Primeiro termo: "))
 Razao = int(input("Qual a razão: " ))
