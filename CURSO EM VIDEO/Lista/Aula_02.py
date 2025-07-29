322b99766e011cf037ca4a51d94ccec8d8199535
Souzadfs <souzadf09@gmail.com>
2025-07-29 05:26:12 -0300

    Add os método de lista

--- CURSO EM VIDEO/Lista/Aula_02.py
+++ CURSO EM VIDEO/Lista/Aula_02.py
@@ -1,3 +1,18 @@
+#Método	Descrição
+#append(x)	Adiciona o item x ao final da lista.
+#insert(i, x)	Insere o item x na posição i.
+#extend(lista)	Adiciona todos os elementos de outra lista.
+#remove(x)	Remove a primeira ocorrência do valor x.
+#pop(i)	Remove e retorna o item da posição i (ou o último se não for especificado).
+#clear()	Remove todos os itens da lista.
+#index(x)	Retorna o índice da primeira ocorrência de x.
+#count(x)	Conta quantas vezes o valor x aparece na lista.
+#sort()	Ordena a lista em ordem crescente.
+#reverse()	Inverte a ordem dos elementos da lista.
+#copy()	Retorna uma cópia superficial da lista.
+
+
+
 lista = [10, 20, 30, 40, 50, 60]
 lista.append ('Luiz')
 #nome = lista.pop()
