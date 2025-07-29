46f9eea20d94192feff038e8a640e1511d7e86fc
Souzadfs <souzadf09@gmail.com>
2025-07-29 05:50:18 -0300

    Aprendendo lista.copy

--- CURSO EM VIDEO/Lista/Aula_03.py
+++ CURSO EM VIDEO/Lista/Aula_03.py
@@ -1,7 +1,15 @@
-lista_a = [1, 2, 3, 4]
-lista_b = [5, 6, 7, 8]
+#lista_a = [1, 2, 3, 4]
+#lista_b = [5, 6, 7, 8]
 
-lista_c = lista_a + lista_b
-lista_a.extend(lista_b)
-print(lista_a)
+#lista_c = lista_a + lista_b
+#lista_a.extend(lista_b)
+#print(lista_a)
+#print(lista_c)
+
+
+lista_a = ['Denis', 'Ana', True, 12, 13]
+lista_b = ['Lara', 17, 22]
+lista_b[0] = ['Outra coisa']
+lista_c = lista_a.copy()
 print(lista_c)
+print(lista_b)
